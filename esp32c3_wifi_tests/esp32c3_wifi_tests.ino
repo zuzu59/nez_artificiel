ad1c9b156a1406a9f6324b7910f7cc0a9c039a28
Christian Zufferey <christian@zufferey.com>
2024-03-11 14:53:25 +0100

    refact: voilà c'est bon, mon test de connexion WIFI fonctionne bien maintenant \o/

--- esp32c3_wifi_tests/esp32c3_wifi_tests.ino
+++ esp32c3_wifi_tests/esp32c3_wifi_tests.ino
@@ -1,6 +1,6 @@
 // Juste un test pour voir comment se connecter en WIFI avec un esp32-c3
 // et au passage Graphe tout simplement sur le serial plotter tool de l'Arduino IDE
-// zf240311.1431
+// zf240311.1432
 //
 // Sources:
 // https://github.com/Seeed-Studio/Seeed_Arduino_MultiGas/blob/master/examples/demo_background/demo_background.ino
@@ -19,9 +19,6 @@ GAS_GMXXX<TwoWire> gas;
 
 static void ConnectWiFi() {
     USBSerial.printf("WIFI_SSID: %s\nWIFI_PASSWORD: %s\n", WIFI_SSID, WIFI_PASSWORD);
-    // USBSerial.println(WIFI_SSID);
-    // USBSerial.print("WIFI_PASSWORD: ");
-    // USBSerial.println(WIFI_PASSWORD);
     WiFi.mode(WIFI_STA); //Optional
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     USBSerial.println("\nConnecting");
@@ -30,11 +27,17 @@ static void ConnectWiFi() {
         delay(100);
     }
     USBSerial.println("\nConnected to the WiFi network");
-    USBSerial.print("Local ESP32 IP: ");
-    USBSerial.println(WiFi.localIP());
+    USBSerial.printf("Local ESP32 IP: %s\n\n", WiFi.localIP());
 }
 
-
+static void Setting_Gaz_Sensor() {
+    Wire.begin(4, 5);     // J'ai branché mon sensor sur les pins 4 et 5 de mon esp32c3 !
+    // If you have changed the I2C address of gas sensor, you must to be specify the address of I2C.
+    //The default addrss is 0x08;
+    gas.begin(Wire, 0x08); // use the hardware I2C
+    //gas.begin(MyWire, 0x08); // use the software I2C
+    //gas.setAddress(0x64); change thee I2C address
+}
 
 void setup() {
     USBSerial.begin(19200);
@@ -43,55 +46,18 @@ void setup() {
     delay(2000);  //le temps de passer sur la Serial Monitor ;-)
 
     ConnectWiFi();
-
-
-
-
-
-    // Wire.begin(4, 5);     // J'ai branché mon sensor sur les pins 4 et 5 de mon esp32c3 !
-    // // If you have changed the I2C address of gas sensor, you must to be specify the address of I2C.
-    // //The default addrss is 0x08;
-    // gas.begin(Wire, 0x08); // use the hardware I2C
-    // //gas.begin(MyWire, 0x08); // use the software I2C
-    // //gas.setAddress(0x64); change thee I2C address
+    Setting_Gaz_Sensor();
 }
 
 void loop() {
-    int NO2 = 0;
-    int C2H5OH = 0;
-    int VOC = 0;
-    int CO = 0;
+    int NO2 = gas.measure_NO2();
+    int C2H5OH = gas.measure_C2H5OH(); 
+    int VOC = gas.measure_VOC(); 
+    int CO = gas.measure_CO(); 
 
-    // NO2 = gas.measure_NO2(); 
-    // USBSerial.print("NO2:"); 
-    // USBSerial.print(NO2); 
-    // USBSerial.print(",");
-    // // USBSerial.print(gas.calcVol(val)); 
-    // // USBSerial.println("V");
-
-    // C2H5OH = gas.measure_C2H5OH(); 
-    // USBSerial.print("C2H5OH:"); 
-    // USBSerial.print(C2H5OH); 
-    // USBSerial.print(",");
-    // // USBSerial.print(gas.calcVol(val)); 
-    // // USBSerial.println("V");
-
-    // VOC = gas.measure_VOC(); 
-    // USBSerial.print("VOC:"); 
-    // USBSerial.print(VOC); 
-    // USBSerial.print(",");
-    // // USBSerial.print(gas.calcVol(val)); 
-    // // USBSerial.println("V");
-
-    // CO = gas.measure_CO(); 
-    // USBSerial.print("CO:"); 
-    // USBSerial.print(CO); 
-    // USBSerial.print(",");
-    // // USBSerial.print(gas.calcVol(val)); 
-    // // USBSerial.println("V");
-
-    // // USBSerial.println("");
-    delay(1000);
+    // Plot sur le serial plotter de l'Arduino IDE
+    USBSerial.printf("NO2:%d,C2H5OH:%d,VOC:%d,CO:%d\n", NO2, C2H5OH, VOC, CO);
+    delay(100);
 }
 
 
