1a655c453a598d50eecae77c1dd1ad33cc246f8f
Christian Zufferey <christian@zufferey.com>
2024-03-11 18:30:23 +0100

    new: ajouté les tests pour MQTT. ATTENTION, cela plante grave pour l'instant

---
+++ esp32c3_mqtt_tests/esp32c3_mqtt_tests.ino
@@ -0,0 +1,117 @@
+// Juste un test pour voir comment envoyer des choses sur un 
+// brocker MQTT compatible Home Assistant avec un esp32-c3
+// et au passage graphe le senseur de gaz tout simplement 
+// sur le serial plotter tool de l'Arduino IDE
+// zf240311.1510
+//
+// Installation:
+// Pour MQTT, il faut installer la lib:
+// https://github.com/dawidchyrzynski/arduino-home-assistant
+//
+// Sources:
+// https://github.com/Seeed-Studio/Seeed_Arduino_MultiGas/blob/master/examples/demo_background/demo_background.ino
+// https://github.com/dawidchyrzynski/arduino-home-assistant/blob/main/examples/sensor-integer/sensor-integer.ino
+
+// WIFI
+#include <WiFi.h>
+#include "secrets.h"
+
+// MQTT
+#include <ArduinoHA.h>
+#define BROKER_ADDR       IPAddress(192,168,0,42)
+#define PUBLISH_INTERVAL  1000 // how often image should be published to HA (milliseconds)
+WiFiClient client;
+HADevice device;
+HAMqtt mqtt("client", device);
+unsigned long lastUpdateAt = 0;
+// "gazMulti" is unique ID of the sensor. You should define your own ID.
+HASensorNumber uptimeSensor("gazMulti");
+
+// Senseur de gaz
+#include <Wire.h>
+#include <Multichannel_Gas_GMXXX.h>
+GAS_GMXXX<TwoWire> gas;
+
+
+static void ConnectWiFi() {
+    USBSerial.printf("WIFI_SSID: %s\nWIFI_PASSWORD: %s\n", WIFI_SSID, WIFI_PASSWORD);
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.setSleep(false);
+    USBSerial.println("\nConnecting");
+    while(WiFi.status() != WL_CONNECTED){
+        USBSerial.print(".");
+        delay(100);
+    }
+    USBSerial.println("\nConnected to the WiFi network");
+    USBSerial.print("Local ESP32 IP: ");
+    USBSerial.println(WiFi.localIP());
+}
+
+
+
+
+static void ConnectMQTT() {
+    // set device's details (optional)
+    device.setName("Arduino");
+    device.setSoftwareVersion("1.0.0");
+    // configure sensor (optional)
+    uptimeSensor.setIcon("mdi:home");
+    uptimeSensor.setName("gazMulti");
+    uptimeSensor.setUnitOfMeasurement("ppm");
+    mqtt.begin(BROKER_ADDR);
+}
+
+
+
+
+static void Setting_Gaz_Sensor() {
+    Wire.begin(4, 5);     // J'ai branché mon sensor sur les pins 4 et 5 de mon esp32c3 !
+    // If you have changed the I2C address of gas sensor, you must to be specify the address of I2C.
+    //The default addrss is 0x08;
+    gas.begin(Wire, 0x08); // use the hardware I2C
+    //gas.begin(MyWire, 0x08); // use the software I2C
+    //gas.setAddress(0x64); change thee I2C address
+}
+
+void setup() {
+    USBSerial.begin(19200);
+    delay(3000);  //le temps de passer sur la Serial Monitor ;-)
+    USBSerial.println("\n\n\n\nCa commence !\n");
+    delay(2000);  //le temps de passer sur la Serial Monitor ;-)
+
+    USBSerial.println("\n\nConnect WIFI !\n");
+    ConnectWiFi();
+
+    USBSerial.println("\n\nConnect MQTT !\n");
+    ConnectMQTT();
+
+    USBSerial.println("\n\nSetting gaz sensor !\n");
+    Setting_Gaz_Sensor();
+}
+
+void loop() {
+    int NO2 = gas.measure_NO2();
+    int C2H5OH = gas.measure_C2H5OH(); 
+    int VOC = gas.measure_VOC(); 
+    int CO = gas.measure_CO(); 
+
+    mqtt.loop();
+
+    if ((millis() - lastUpdateAt) > 2000) { // update in 2s interval
+        unsigned long uptimeValue = millis() / 1000;
+        // uptimeSensor.setValue(uptimeValue);
+        lastUpdateAt = millis();
+        // you can reset the sensor as follows:
+        // analogSensor.setValue(nullptr);
+    }
+
+
+
+    // Plot sur le serial plotter de l'Arduino IDE
+    // USBSerial.printf("NO2:%d,C2H5OH:%d,VOC:%d,CO:%d\n", NO2, C2H5OH, VOC, CO);
+    delay(1000);
+}
+
+
+
