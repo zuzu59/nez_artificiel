94f256bd72ad1f501a58630532a61381e3a6cb65
Christian Zufferey <christian@zufferey.com>
2024-03-11 11:54:47 +0100

    new: ajouté la gestion des secrets pour le WIFI et autres

---
+++ secrets_exemple.h
@@ -0,0 +1,9 @@
+// zf240311.1151
+// gestion des secrets pour ce projet
+// faire un :
+// cp secrets_exemple.h secrets.h
+// et modifier secrets.h en conséquence
+
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
+
