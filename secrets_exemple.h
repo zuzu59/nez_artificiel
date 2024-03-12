f5a3fe92cc5d515a8ac57f172661eac515da6225
Christian Zufferey <christian@zufferey.com>
2024-03-12 17:33:23 +0100

    refact: refactorisé le fichier secrets exemple pour ajouter la partie mqtt

--- secrets_exemple.h
+++ secrets_exemple.h
@@ -1,9 +1,14 @@
-// zf240311.1417
+// zf240312.1732
 // gestion des secrets pour ce projet
 // faire un :
 // cp secrets_exemple.h projet/secrets.h
 // et modifier secrets.h en conséquence
 
-#define WIFI_SSID "MySSID"		// replace MySSID with your WiFi network name
-#define WIFI_PASSWORD "MyPassword"	// replace MyPassword with your WiFi password
+#define WIFI_SSID "xxx"		// replace MySSID with your WiFi network name
+#define WIFI_PASSWORD "xxx"	// replace MyPassword with your WiFi password
+
+#define BROKER_ADDR IPAddress(192,168,0,xx)
+#define BROKER_USERNAME "xxx"
+#define BROKER_PASSWORD "xxx"
+
 
